f0dbae6b32abdbfa1c5a90bc85b1ef9df396fdeb
Bryan Valcasara <bryanvalcasara@MacBook-Air-de-Bryan.local>
2019-09-19 17:28:51 +0200

    ajout quizz

---
+++ Open_Quizz/Model/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Open_Quizz
+//
+//  Created by Bryan Valcasara on 23/03/2019.
+//  Copyright © 2019 Bryan Valcasara. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    var title = ""
+    var isCorrect = false
+}
